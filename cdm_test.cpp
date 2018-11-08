37eaa976d98c0ab1f108ccdc261da14ead6b94d2
Marek Moeckel <marekmoeckel@serc.org.au>
2018-11-08 13:58:56 +1100

    Update example

--- cdm_test.cpp
+++ cdm_test.cpp
@@ -11,7 +11,11 @@ int main(int argc, char *argv[])
     cdm.setObject2Metadata("50129", "2020-014A", "EOSCAT", "PACMAN", "NONE", CDM::DEFAULT, CDM::NOT_MANEUVERABLE, CDM::GCRF);
     cdm.setObject1StateVector(9800.0,1277.3,868.22,0.01,0.67,7.34,"Not a proper orbit but considered valid");
     cdm.setObject2StateVector(9800.0,1277.3,868.22,0.01,0.67,7.34,"Not a proper orbit but considered valid");
-    std::cout << cdm.toKVN();
+    cdm.setRelativePosition(3.5, 6.7, 1.1);
+    //std::cout << cdm.toKVN();
+
+    ConjunctionDataMessage cdm2(cdm.toProtobufString());
+    std::cout << cdm2.toKVN();
     //std::cout << cdm.toJSON() << std::endl;
     return 0;
 }
